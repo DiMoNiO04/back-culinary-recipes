3b935f1c47bdeefee64fa917b34c026dec2abe30
DiMoNiO04 <dima.razumov.940@mail.ru>
2024-10-06 10:00:25 +0300

    feat: add auth functional

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,25 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { ApiOperation, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { AuthService } from './auth.service';
+import { User } from 'src/users/users.model';
+
+@ApiTags('Аутентификация')
+@Controller('auth')
+export class AuthController {
+  constructor(private authService: AuthService) {}
+
+  @ApiOperation({ summary: 'Авторизация' })
+  @ApiResponse({ status: 200, type: [User] })
+  @Post('/login')
+  login(@Body() userDto: CreateUserDto) {
+    return this.authService.login(userDto);
+  }
+
+  @ApiOperation({ summary: 'Регистрация' })
+  @ApiResponse({ status: 201, type: [User] })
+  @Post('/registration')
+  registration(@Body() userDto: CreateUserDto) {
+    return this.authService.registration(userDto);
+  }
+}
